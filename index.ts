932e7ad1272a45272123287103c0299e18f71693
Strider <strange.transistor@gmail.com>
2021-08-23 01:11:12 +0300

    match trees.

--- index.ts
+++ index.ts
@@ -4,32 +4,46 @@ import Reader from './lib/Reader'
 import { read } from './lib/read'
 // import { read_until } from './lib/read'
 
+import { $Matcher } from './lib/matcher'
 import { Literal } from './lib/matcher'
 import { Charclass } from './lib/matcher'
 import { Num } from './lib/matcher'
 import { Line } from './lib/matcher'
+import { Optional } from './lib/matcher'
 import { Seq } from './lib/matcher'
 import { OneOf } from './lib/matcher'
+import { Portal } from './lib/matcher'
 
 var plus = Literal('+')
 var space = Charclass('\\s')
 var number = Num()
 var sep = Literal(';')
-var line = Line()
+// var line = Line()
 
-// var expr = OneOf(sum, expr)
-// var program = Seq(expr)
+function Spaced <T = string> (matcher: $Matcher<T>)
+{
+	return Seq(Optional(space), matcher, Optional(space))
+}
 
-// var r = Reader('abc')
-var r = Reader(`123  +   345; 111+222`)
+var expr_portal = Portal()
 
-// var m = Seq(number, space)
-var m = OneOf(number, line)
+var binop = Seq(number, Spaced(plus), expr_portal)
 
-var P = m(r)
+var expr = OneOf(
+	binop,
+	number,
+)
+
+var program = Spaced(expr)
+
+expr_portal.onto = expr
+
+var r = Reader(`123  +   345`)
+
+var P = expr(r)
 // console.info(P)
 console.info(P.reader.repr())
-console.info(P.is_nothing || P.match)
+console.dir(P.is_nothing || P.match, { depth: Infinity })
 
 // console.log(read(r, 1).map(() => 1))
 // console.log(read(r, 3).map(() => 2))
--- lib/matcher.ts
+++ lib/matcher.ts
@@ -65,6 +65,23 @@ export function Line (): $Matcher
 	}
 }
 
+export function Optional <T = string> (matcher: $Matcher<T>): $Matcher<T | null>
+{
+	return (reader) =>
+	{
+		var P = matcher(reader)
+
+		if (P.is_nothing)
+		{
+			return Matched(reader, null)
+		}
+		else
+		{
+			return P
+		}
+	}
+}
+
 export function Seq <Out extends any[]> (...matchers: { [Index in keyof Out]: $Matcher<Out[Index]> }): $Matcher<Out>
 {
 	return (reader) =>
@@ -73,12 +90,13 @@ export function Seq <Out extends any[]> (...matchers: { [Index in keyof Out]: $M
 		var P
 		var R = []
 
-		for (var matcher of matchers)
+		for (var [ matcher, index1 ] of enumerate(matchers, 1))
 		{
 			P = matcher(next_reader)
 
 			if (P.is_nothing)
 			{
+				console.warn('Seq', index1, matcher)
 				return Nothing(reader)
 			}
 
@@ -98,16 +116,43 @@ export function OneOf <Out extends any[]> (...matchers: { [Index in keyof Out]: 
 	{
 		var P
 
-		for (var matcher of matchers)
+		for (var [ matcher, index1 ] of enumerate(matchers, 1))
 		{
 			P = matcher(reader)
 
 			if (! P.is_nothing)
 			{
+				console.warn('OneOf', index1, matcher)
 				return P
 			}
 		}
 
 		return Nothing(reader)
+	}
+}
+
+type $Portal <T = string> = $Matcher<T> & { onto: $Matcher<T> | null }
+
+export function Portal <T> (): $Portal<T>
+{
+	function portal (reader: $Reader)
+	{
+		if (! portal.onto) throw new TypeError('no_portal_onto')
+
+		return portal.onto(reader)
+	}
+
+	portal.onto = null as any
+
+	return portal
+}
+
+
+function * enumerate <T> (iterator: Iterable<T>, start_from = 0): Iterable<[ T, number ]>
+{
+	var n = start_from
+	for (var value of iterator)
+	{
+		yield [ value, n++ ]
 	}
 }
